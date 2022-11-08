6debb8a61799d4c625e5822d391d12910b0f4e34
jasonzzhao <jzhao0618@gmail.com>
2022-11-08 14:17:17 -0600

    deleted type mismatching compound boolean

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MaybeMecanum.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MaybeMecanum.java
@@ -170,11 +170,11 @@ public class MaybeMecanum extends OpMode
             rr*=.7;
         }
 
-        if(controller.dpadUpOnce()&&robot.lchain.getPosition()){
+        if(controller.dpadUpOnce()){
             robot.lchain.setPosition(robot.lchain.getPosition()+0.05);
         }
 
-        if(controller.dpadDownOnce()&&robot.lchain.getPosition()){
+        if(controller.dpadDownOnce()){
             robot.lchain.setPosition(robot.lchain.getPosition()-0.05);
         }
 
